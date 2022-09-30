b0f547007cb796eff105c6dafe31dd0b67ce3107
solightnsalt <soldakm@gmail.com>
2022-09-30 13:05:05 +0900

    ds to kp

--- pair/crud/urls.py
+++ pair/crud/urls.py
@@ -5,6 +5,6 @@ app_name = "crud"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create, name="create"),
-    path("detail/<pk>", views.detail, name="detail"),
+    path("create/", views.create, name="create"),
+    path("detail/", views.detail, name="detail"),
 ]
